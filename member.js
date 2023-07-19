d9567e7a815869c464a656432a143b4491dab1b4
llander2 <139920148+llander2@users.noreply.github.com>
2023-07-19 22:36:17 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember()
+{
+    var member = document.getElementById("member").value;
+    var member2 = document.getElementById("member2").value;
+    var total = member + member2;
+    return total;
+}
