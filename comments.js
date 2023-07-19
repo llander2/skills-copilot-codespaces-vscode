562ebf0dc2feaa24da8c0ae1172080a21a2efa55
llander2 <139920148+llander2@users.noreply.github.com>
2023-07-19 22:39:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,51 @@
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World\n");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.end("<html><body><h1>Hello World</h1></body></html>");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.end("<html><body><h1>Hello World</h1></body></html>");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.end("<html><body><h1>Hello World</h1></body></html>");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.end("<html><body><h1>Hello World</h1></body></html>");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.end("<html><body><h1>Hello World</h1></body></html>");
+// });
+// server.listen(8000);
+// console.log("Server running at http://localhost:8000/");
+// create web server
+// var http = require('http');
+// var server = http
